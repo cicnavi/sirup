5c63c31c1e7cd3448f8e7a1caa25b0b13990affe
Marko Ivančić <marko.ivancic@srce.hr>
2020-02-27 00:31:03 +0100

    WIP UI

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# sirup
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+# Sirup
+Web app for syrup dosage calculation based on the weight of the child, related to commonly available syrups available
+in Croatia.
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="bg-light">
+      <div class="container">
+        <div class="py-1 text-center">
+          <img class="d-block mx-auto mb-4"
+               src="./assets/logo.png"
+               alt=""
+               width="72"
+               height="72">
+          <h2>Sirup</h2>
+          <p class="lead">
+            <small>
+              Izračun doze sirupa (ml) prema kilaži djeteta
+            </small>
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col"></div>
+          <div class="col-2">
+            <label for="firstName" class="lead">Kilaža</label>
+            <input type="number"
+                   min="3"
+                   max="100"
+                   step="0.1"
+                   class="form-control form-control-lg"
+                   id="kilaza"
+                   placeholder="Unesite kilažu (broj)"
+                   value="13.5"
+                   required
+                   style="text-align: center;">
+          </div>
+          <div class="col"></div>
+        </div>
+
+        <div class="m-4 text-warning">
+          TODO mivanci prikaz greške
+        </div>
+
+        <div class="row mt-3">
+          <div class="col">
+            <h4 class="mb-3">Paracetamol</h4>
+          </div>
+
+          <div class="col">
+            <h4>Ibuprofen</h4>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            TODO mivanci općenite informacije o temperaturi
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <p>
+              TODO mivanci odricanje od odgovornosti
+            </p>
+          </div>
+        </div>
+
+        <footer class="my-5 pt-5 text-muted text-center text-small">
+          <p class="mb-1">
+            &copy; Sirup
+          </p>
+          <p>
+            <small>
+              Ikone izradila <a href="https://www.flaticon.com/authors/nikita-golubev"
+                                title="Nikita Golubev">Nikita Golubev</a>, dostupno na
+              <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            </small>
+          </p>
+        </footer>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'Sirup',
+        components: {}
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 10px;
+  }
 </style>
